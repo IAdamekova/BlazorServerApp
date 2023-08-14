8fdd26ecc8e5c505efc1a2252abd734b12e7572a
ITimkova <ivanka21timkova@gmail.com>
2023-08-14 20:27:50 +0200

    Initial commit

---
+++ BlazorServerApp/Program.cs
@@ -0,0 +1,41 @@
+using BlazorServerApp.Data;
+using BlazorServerApp.Models;
+using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMatBlazor();
+
+
+// we had to add code below!!!
+builder.Services.AddDbContext<AdventureWorks2019Context>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
